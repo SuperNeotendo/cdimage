d3015f6cb4d86126376bda8792be219c0cc1ff92
Mario <45854100+SuperNeotendo@users.noreply.github.com>
2025-08-15 01:30:10 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// A tool for burning visible pictures on a compact disc surfase
+// Copyright (C) 2008-2022 arduinocelentano
+
+//This program is free software: you can redistribute it and/or modify it under the terms
+//of the GNU General Public License as published by the Free Software Foundation,
+//either version 3 of the License, or (at your option) any later version.
+
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//See the GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License along with this program.
+//If not, see <https://www.gnu.org/licenses/>.
+
+//main
+
+#include "mainwindow.h"
+#include <QApplication>
+
+
+double input_tr, input_dtr; // What??!!! Global variables?
+
+int main ( int argc, char** argv )
+{
+    if (argc != 3) printf("Usage: cdmage tr dtr\n");
+    input_tr = atof(argv[1]);
+    input_dtr = atof(argv[2]);
+    QApplication app ( argc, argv );
+    MainWindow win;
+    win.setWindowTitle("CD-Image Version: 1.1");
+    win.show();
+    return app.exec();
+}
+
